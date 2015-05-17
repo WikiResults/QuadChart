dc0fab2e01e5df53806f8ff1af263af6354f0e38
NEO <ramnathbabutj@gmail.com>
2015-05-18 03:56:05 +0530

    Added a simple Quad-chat display in quadChart.php - nothing dynamic yet!

---
+++ quadChart.php
@@ -0,0 +1,37 @@
+<?php
+	// nothing dynamic yet!
+?><html>
+	<head>
+		<title>QUad Chart</title>
+	</head>
+	<body>a
+		<h1>Quad Charts!</h1>
+		<form>
+			<table>
+				<tr>
+					<td>
+						<textarea name="topLeft" rows="4" cols="50" value="Things to be done this week">
+						</textarea>
+					</td>
+					<td>
+						<textarea name="topRight" rows="4" cols="50" value="Things to be done this week">
+						</textarea>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<textarea name="bottomLeft" rows="4" cols="50" value="Things to be done this week">
+						</textarea>
+					</td>
+					<td>
+						<textarea name="bottomRight" rows="4" cols="50" value="Things to be done this week">
+						</textarea>
+					</td>
+				</tr>
+				<!-- and more rows here ... -->
+			</table>
+			<input type="submit" value="Save" />
+		</form>
+	</body>
+</html>
+
